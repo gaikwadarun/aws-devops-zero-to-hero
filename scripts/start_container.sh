bbdf9b2a021fad3afc278fc954af0de71112a2a9
gaikwadarun <118682233+gaikwadarun@users.noreply.github.com>
2024-11-30 22:53:37 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull arun888/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 arun888/simple-python-flask-app
+docker run -d -p 8000:8000 arun888/simple-python-flask-app
